e439db088a5ae1da667600ea078f66487a0b41ba
Zhuraveln <nikitazuravel@Nikitas-MacBook-Pro.local>
2022-09-13 20:26:47 +0300

    Ver 0.2 Refactor CSS, New design, Add progress-bar for habbits

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const habbitsCards = document.querySelector('.habbits-cards')
+const habbitsCards = document.querySelector('#habbits-cards')
 
 const formForHabbit = document.querySelector('#form-for-habbit')
 
@@ -11,20 +11,27 @@ let habbits = getLocal()
 
 function toCard(habbit, index) {
   return `
-        <div class="habbit-card ${habbit.completed ? 'checked' : ''}">
-            <div class="top">
-                <div class="description">${habbit.description}</div>
-                <div class="top-buttons">
-                    <input onclick="completehabbit(${index})" type="checkbox" 
-                    class="btn-complete" ${habbit.completed ? 'checked' : ''}>
-                    <button onclick="changehabbit(${index})" class="btn-change">Change</button>
-                </div>
-            </div>
-            <div class="bottom">
-                <p class="post-day">${habbit.date}</p>
-                <button onclick="deleteHabbit(${index})" class="btn-delete">Delete</button>
-            </div>
-        </div>`
+    <div class="habbit-card ${habbit.completed ? 'checked' : ''}">
+      <div class="card">
+        <div class="top">
+          <div class="description">${habbit.description}</div>
+          <div class="top-buttons">
+            <!-- <input onclick="completehabbit(${index})" type="checkbox" 
+            class="btn-complete" ${habbit.completed ? 'checked' : ''}>
+            <button class="btn" onclick="changehabbit(${index})" class="btn-change">Change</button> -->
+            <button class="btn" onclick="doneHabbit(${index})">Done</button>
+          </div>
+        </div>
+        <div class="bottom">
+          <p class="post-day">${habbit.date}</p>
+          <button class="btn-delete" onclick="deleteHabbit(${index})">Delete</button>
+        </div>
+      </div>
+      <div class="progress-bar">
+        <div class="progress-value"></div>
+      </div>
+    </div>
+  `
 }
 
 // const filterhabbits = () => {
@@ -37,12 +44,11 @@ function toCard(habbit, index) {
 
 function renderHtmlList() {
   renderHabbitsCards()
-  // renderHabbitsProgress()
 }
 
 function renderHabbitsCards() {
   if (habbits.length === 0) {
-    habbitsCards.innerHTML = `<p>You have not tracked habbit, add</p>`
+    habbitsCards.innerHTML = `<p class="empty">You have not tracked habbit, add</p>`
   } else {
     let html = ''
     // filterhabbits()
@@ -51,14 +57,20 @@ function renderHabbitsCards() {
       html += toCard(item, index)
     })
     habbitsCards.innerHTML = html
-    // habbits = document.querySelectorAll('.habbit-card')
+    renderHabbitsProgress()
   }
 }
 
-// renderHabbitsProgress()
+function renderHabbitsProgress() {
+  const progressValue = document.querySelectorAll('.progress-value')
+  progressValue.forEach((item, index) => {
+    item.style.width = habbits[index].progress + '0%'
+  })
+}
 
 function Habbit(description) {
   this.description = description
+  this.progress = 0
   this.completed = false
   this.date = getDate()
 }
@@ -115,33 +127,39 @@ function addHabbit(event) {
   renderHtmlList()
 }
 
-function completehabbit(index) {
-  habbits[index].completed = !habbits[index].completed
-  if (habbits[index].completed) {
-    habbits[index].classList.add('checked')
-  } else {
-    habbits[index].classList.remove('checked')
-  }
+function doneHabbit(index) {
+  habbits[index].progress++
   saveLocal()
   renderHtmlList()
 }
 
-function changehabbit(index) {
-  if (
-    habbits[index].children[1].children[1].classList.value ===
-    'btn-delete hide'
-  ) {
-    habbits[index].children[1].children[1].classList.remove('hide')
-    habbits[index].children[0].children[1].children[1].classList.add(
-      'clicked'
-    )
-  } else {
-    habbits[index].children[1].children[1].classList.add('hide')
-    habbits[index].children[0].children[1].children[1].classList.remove(
-      'clicked'
-    )
-  }
-}
+// function completehabbit(index) {
+//   habbits[index].completed = !habbits[index].completed
+//   if (habbits[index].completed) {
+//     habbits[index].classList.add('checked')
+//   } else {
+//     habbits[index].classList.remove('checked')
+//   }
+//   saveLocal()
+//   renderHtmlList()
+// }
+
+// function changehabbit(index) {
+//   if (
+//     habbits[index].children[1].children[1].classList.value ===
+//     'btn-delete hide'
+//   ) {
+//     habbits[index].children[1].children[1].classList.remove('hide')
+//     habbits[index].children[0].children[1].children[1].classList.add(
+//       'clicked'
+//     )
+//   } else {
+//     habbits[index].children[1].children[1].classList.add('hide')
+//     habbits[index].children[0].children[1].children[1].classList.remove(
+//       'clicked'
+//     )
+//   }
+// }
 
 function deleteHabbit(index) {
   // habbitss[index].classList.add('delition')
