c68d7837c8325d1e2633903793a738b48ae634d6
Zhuraveln <nikitazuravel@Nikitas-MacBook-Pro.local>
2022-10-08 21:13:38 +0300

    Fixed env to Vercel 4

--- client/src/components/HabbitForm.jsx
+++ client/src/components/HabbitForm.jsx
@@ -8,7 +8,7 @@ const HabbitForm = ({ create }) => {
   const addNewHabbit = async (e) => {
     e.preventDefault()
 
-    const { data } = await axios.post(`${process.env.REACT_APP_API_URL}random`, habbit)
+    const { data } = await axios.post(process.env.REACT_APP_API_URL, habbit)
 
     create(data)
 
@@ -25,7 +25,7 @@ const HabbitForm = ({ create }) => {
   // }
 
   const fetchTrashHabbit = async () => {
-    const response = await axios.get(process.env.REACT_APP_API_URL)
+    const response = await axios.get(`${process.env.REACT_APP_API_URL}random`)
     setTrashHabbits(response.data)
   }
 
